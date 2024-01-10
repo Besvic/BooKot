8908114b89097a94b50005afc59737fbd0e5a78e
vbesarab <besarab.victor20@gmail.com>
2024-01-10 15:31:20 +0300

    Initial commit

---
+++ src/main/kotlin/com/online/bookot/BooKotApplication.kt
@@ -0,0 +1,11 @@
+package com.online.bookot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BooKotApplication
+
+fun main(args: Array<String>) {
+    runApplication<BooKotApplication>(*args)
+}
---
+++ src/test/kotlin/com/online/bookot/BooKotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.online.bookot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BooKotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
